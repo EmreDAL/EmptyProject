01dc03509ae811840840a4057d62c45cdfc459e1
EmreDAL <emrcn3452@hotmail.com>
2017-05-07 03:42:52 +0300

    Update lib.js

--- lib.js
+++ lib.js
@@ -7,14 +7,13 @@ module.exports = {
     libFolder + "/jquery/dist/jquery.js",
     libFolder + "/magnific-popup/dist/jquery.magnific-popup.js",
     libFolder + "/slick-carousel/slick/slick.js",
+    libFolder + "/isotope/dist/isotope.pkgd.min.js"
   ],
   styleLibFiles: [
     libFolder + "/flexiblegs-css/flexiblegs-css.css",
     libFolder + "/magnific-popup/dist/magnific-popup.css",
     libFolder + "/slick-carousel/slick/slick.css",
     libFolder + "/animate.css/animate.css",
-    "app/Content/css/fontello.css",
-    "app/Content/css/fontello-ie7.css",
-    "app/Content/css/iconfonts.min.css"
+    libFolder + "/font-awesome/css/font-awesome.min.css"
   ]
 };
